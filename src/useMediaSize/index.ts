5ff5ebb7aad810fdd3f0b90c1426194480f78e59
James Gee <geemanjs@gmail.com>
2020-05-04 21:23:35 +0100

    bump version

--- src/useMediaSize/index.ts
+++ src/useMediaSize/index.ts
@@ -4,10 +4,19 @@ import { MediaSizesState } from "../useMediaSizeQueries/MediaSizeQueriesContextP
 
 export const useMediaSize = (overrideSizes?: MediaSizesState) => {
   const defaultSizes = useMediaSizeQueries();
+  let joinedSizes;
+  if(!overrideSizes) {
+    joinedSizes = defaultSizes;
+  } else {
+    joinedSizes = {
+      ...defaultSizes,
+      ...overrideSizes
+    }
+  }
   return {
-    isSm: useMediaQuery(`(max-width: ${overrideSizes.sm || defaultSizes.sm})`),
-    isMd: useMediaQuery(`(max-width: ${overrideSizes.md || defaultSizes.md})`),
-    isLg: useMediaQuery(`(max-width: ${overrideSizes.lg || defaultSizes.lg})`),
-    isXl: useMediaQuery(`(max-width: ${overrideSizes.xl || defaultSizes.xl})`),
+    isSm: useMediaQuery(`(max-width: ${joinedSizes.sm})`),
+    isMd: useMediaQuery(`(max-width: ${joinedSizes.md})`),
+    isLg: useMediaQuery(`(max-width: ${joinedSizes.lg})`),
+    isXl: useMediaQuery(`(max-width: ${joinedSizes.xl})`),
   };
 };
